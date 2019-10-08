233bf1dbf224d18886a5aab29e35ee18501104ce
Paco <kkdh7609@ajou.ac.kr>
2019-10-09 01:38:53 +0900

    typo correct

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -13,13 +13,13 @@ class _MyAppState extends State<MyApp>{
   Widget _screen;
   login _login;
   settings _settings;
-  bool _authent;
+  bool _authenticated;
 
   _MyAppState(){
     _login = new login(onSubmit: (){onSubmit();});
     _settings = new settings();
     _screen = _login;
-    _authent = false;
+    _authenticated = false;
   }
 
   void onSubmit(){
@@ -33,7 +33,7 @@ class _MyAppState extends State<MyApp>{
   void _goHome(){
     print("go home");
     setState(() {
-      if (_authent == true) {
+      if (_authenticated == true) {
         _screen = _settings;
       }
       else {
@@ -49,11 +49,12 @@ class _MyAppState extends State<MyApp>{
   void _setAuthenticated(bool auth){
     setState(() {
       if (auth == true) {
-        _authent = true;
+        _authenticated = true;
         _screen = _login;
         _title = "Welcome";
       }
       else {
+        _authenticated = false;
         _screen = _login;
         _title = "Please Login";
       }
