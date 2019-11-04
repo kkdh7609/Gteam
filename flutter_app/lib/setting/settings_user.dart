5397c2bc475c7fe5b0cc90fe7146eaf512a39d58
wooil <boxer1532@ajou.ac.kr>
2019-11-04 20:06:25 +0900

    Make Temporary MainMenu && Merge Game_Create/Join

--- flutter_app/lib/setting/settings_user.dart
+++ flutter_app/lib/setting/settings_user.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:gteams/login/login_auth.dart';
 import 'package:gteams/root_page.dart';
+import 'package:gteams/signup/sign_up.dart';
+import 'package:gteams/game/game_create.dart';
+import 'package:gteams/map/google_map.dart';
+import 'package:gteams/game_join/game_join.dart';
 
 class SettingUserPage extends StatefulWidget{
   SettingUserPage({Key key, this.onSignedOut}) : super(key: key);
@@ -15,17 +19,44 @@ class _SettingUserPageState extends State<SettingUserPage>{
   @override
   Widget build(BuildContext context){
     return new Scaffold(
-      appBar: AppBar(title: Text("Setting User"),
-      actions: <Widget>[
-        new IconButton(
-          icon: new Icon(Icons.arrow_back),
-          onPressed: (){
-            widget.onSignedOut();
-            new RootPage(auth: new Auth());
-          }
+        appBar: AppBar(title: Text("Setting"),
+            actions: <Widget>[
+              new IconButton(
+                  icon: new Icon(Icons.arrow_back),
+                  onPressed: (){
+                    widget.onSignedOut();
+                    new RootPage(auth: new Auth());
+                  }
+              )
+            ]),
+        body: Center(
+            child: TempButton()
         )
-      ]),
-      body: Text("setting user page")
+    );
+  }
+}
+
+class TempButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child:Column(
+
+          children: <Widget>[
+            FlatButton(
+              child: Text("게임 생성"),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>GameCreatePage()));
+              },
+            ),
+            FlatButton(
+              child: Text("게임 참여"),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>GameJoinPage()));
+              },
+            )
+          ],
+        )
     );
   }
 }
