ec01adacd7c1d0baf094938957eb63147033e57c
Paco <kkdh7609@ajou.ac.kr>
2019-12-10 07:10:29 +0900

    auto deploy test

---
+++ gteams/deploy.sh
@@ -0,0 +1,11 @@
+export DEPLOY_COMMIT = "$TRAVIS_COMMIT"
+
+SLACK_KEY="xoxp-755490221190-753275073456-868464454406-17e43a7715b1ffee8645f68f6a139317"
+SLACK_TEXT="[\`$DEPLOY_COMMIT\`] ${TRAVIS_COMMIT_MESSAGE:-none} "
+
+curl \
+  -F "token=$SLACK_KEY" \
+  -F "channels=deploy-android" \
+  -F "initial_comment=$SLACK_TEXT" \
+  -F "file=@app/build/outputs/apk/app-debug.apk" \
+  https://slack.com/api/files.upload
