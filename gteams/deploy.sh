26478534a61b3575d17400f89161099b413fbd52
kkdh7609 <kkdh7609@naver.com>
2019-12-10 11:43:12 +0900

    Update deploy.sh

--- gteams/deploy.sh
+++ gteams/deploy.sh
@@ -1,9 +1,9 @@
-SLACK_KEY="xoxp-755490221190-753275073456-868464454406-17e43a7715b1ffee8645f68f6a139317"
+SLACK_KEY="xoxp-755490221190-753275073456-866523130101-6d045dfb7f0933643f372bb36a32b132"
 SLACK_TEXT="New APK"
 
 curl \
   -F "token=$SLACK_KEY" \
   -F "channels=#capstone-project" \
   -F "initial_comment=$SLACK_TEXT" \
-  -F "file=@build/app/outputs/apk/app.apk" \
+  -F "file=@build/app/outputs/apk/release/app-release.apk" \
   https://slack.com/api/files.upload
